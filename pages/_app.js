f085f1bbfda34870e2433ca08e4866cbc08ab213
IAMMiguela <iamworldwidemiguelamanalo@gmail.com>
2025-08-19 16:24:10 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css';
+// /pages/_app.js
+import Link from "next/link";
+import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      {/* Navbar */}
+      <nav style={{ padding: "10px", background: "#222", color: "white" }}>
+        <Link href="/" style={{ marginRight: 15, color: "white" }}>Home</Link>
+        <Link href="/products" style={{ marginRight: 15, color: "white" }}>Products</Link>
+        <Link href="/cart" style={{ marginRight: 15, color: "white" }}>Cart</Link>
+        <Link href="/login" style={{ marginRight: 15, color: "white" }}>Login</Link>
+        <Link href="/register" style={{ marginRight: 15, color: "white" }}>Register</Link>
+      </nav>
+
+      {/* Page content */}
+      <div style={{ padding: "20px" }}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
+
+export default MyApp;
